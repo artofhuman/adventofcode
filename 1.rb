85470fadfcfba55d833c4ee282409e31967ebc04
Semyon Pupkov <mail@semyonpupkov.com>
2017-12-07 17:14:35 +0300

    Add first day part 2

--- 1.rb
+++ 1.rb
@@ -1,3 +1,4 @@
+# part 1
 class Captcha
   def call(input)
     digits = input.chars.map(&:to_i)
@@ -18,7 +19,23 @@ end
 
 cptch = Captcha.new
 
-puts cptch.call('1122').inspect
-puts cptch.call('1111').inspect
-puts cptch.call('1234').inspect
-puts cptch.call('91212129').inspect
+puts cptch.call('1122')
+puts cptch.call('1111')
+puts cptch.call('1234')
+puts cptch.call('91212129')
+
+# part 2
+def solution(input)
+  sum = 0
+  length = input.size
+
+  digits = input.chars.map(&:to_i)
+
+  digits.each.with_index do |digit, i|
+    next_digit = digits[(i + (length / 2)) % length]
+
+    sum += digit if (digit == next_digit)
+  end
+
+  sum
+end
