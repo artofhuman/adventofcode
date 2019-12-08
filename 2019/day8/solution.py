6f162fee04ca8a785e0084cb6d44d66d7a96a22a
Semyon Pupkov <mail@semyonpupkov.com>
2019-12-08 21:37:49 +0500

    Add day8 p2

--- 2019/day8/solution.py
+++ 2019/day8/solution.py
@@ -11,6 +11,10 @@ input = open('input.txt').read().split('\n')[0]
 w = 25
 h = 6
 
+# input = '0222112222120000'
+# w = 2
+# h = 2
+
 layers = []
 layer = []
 
@@ -26,16 +30,60 @@ for i, step in enumerate(range(0, len(input), w)):
 
     print(pixel)
 
-for i, layer in enumerate(layers):
-    line = ''.join(layer)
-    c = collections.Counter(line)
 
-    count_zeros[i] = c['0']
-
-min_line = min(count_zeros, key=count_zeros.get)
+# input = '0222112222120000'
+# w = 2
+# h = 2
 
 
-print(min_line)
-c = collections.Counter(''.join(layers[min_line]))
+def part1():
+    for i, layer in enumerate(layers):
+        line = ''.join(layer)
+        c = collections.Counter(line)
 
-print(c['1'] * c['2'])
+        count_zeros[i] = c['0']
+
+    min_line = min(count_zeros, key=count_zeros.get)
+
+
+    print(min_line)
+    c = collections.Counter(''.join(layers[min_line]))
+
+    print(c['1'] * c['2'])
+
+# part1()
+
+print(layers)
+
+# 0 - black
+# 1 - white
+# 2 - transparent
+
+result = []
+matrix = [['-' for i in range(w)] for i in range(h)]
+
+for layer in layers:
+    for p in layer:
+        print(p)
+
+layers.reverse()
+
+for num_layer, layer in enumerate(layers):
+    print('-------')
+    for num_pixel, pixel in enumerate(layer):
+        line = num_pixel
+
+        for pos_char, char in enumerate(pixel):
+            if char != '2':
+                matrix[line][pos_char] = char
+
+
+for line in matrix:
+    _line = []
+    for ch in line:
+        if ch == '1':
+            _line.append('*')
+        else:
+            _line.append(' ')
+
+    print(''.join(_line))
