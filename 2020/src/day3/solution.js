df94e70ef0460e8e2e6ab4afbdb34373c123b285
Semyon Pupkov <mail@semyonpupkov.com>
2020-12-03 21:08:25 +0500

    day3p2

--- 2020/src/day3/solution.js
+++ 2020/src/day3/solution.js
@@ -3,51 +3,27 @@ import { readFile } from "../stdin.js"
 let testInput = readFile("src/day3/test_input.txt");
 let input = readFile("src/day3/input.txt");
 
-function move(pos, rowNum, input) {
-    const row = input[rowNum]
-    const char = row[pos];
-
-    let newRow = input[rowNum].split('');
-    let hasTree = false;
-
-    if (char === "#") {
-        newRow[pos] = "X";
-        hasTree = true;
-    } else {
-        newRow[pos] = "O";
-    }
-
-    return {
-        "hasTree": hasTree,
-        "row": newRow.join("") // just for debug
-    }
-}
-
-
-function solution(input) {
-    let curPos = 3;
+function solution(input, right, down) {
+    let curPos = 0;
     let countTrees = 0;
+    const width = input[0].length;
 
-    for (let i = 0; i < input.length; i++) {
-        let rowNum = i + 1;
-
-        if (rowNum < input.length) {
-            const result = move(curPos, i + 1, input);
-            if (curPos + 3 >= input[rowNum].length) {
-                curPos = Math.abs(input[rowNum].length - (curPos + 3));
-                rowNum = 0;
-            } else {
-                curPos = curPos + 3;
-            }
-
-            if (result['hasTree']) {
-                countTrees++;
-            }
+    for (let rowNum = 0; rowNum < input.length; rowNum = rowNum + down) {
+        if (input[rowNum][curPos] === "#") {
+            countTrees++;
         }
+        curPos = (curPos + right) % width;
     }
 
     return countTrees;
 }
 
-const result = solution(input);
-console.log("result", result);
+const SLOPES = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+
+let total = 1;
+SLOPES.forEach(function(slope) {
+    const res = solution(input, ...slope);
+    total *= res;
+});
+
+console.log(total);
