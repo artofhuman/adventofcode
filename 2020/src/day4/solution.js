11ea2b7a46fb1551ab77b84d695135b482107bb5
Semyon Pupkov <mail@semyonpupkov.com>
2020-12-05 11:54:33 +0500

    add day4p2

--- 2020/src/day4/solution.js
+++ 2020/src/day4/solution.js
@@ -1,7 +1,5 @@
 import { readFileSync } from 'fs';
 
-
-const CID = "cid";
 const REQUIRED_FIELDS = [
     "byr",
     "iyr",
@@ -17,16 +15,64 @@ function readFile(filePath) {
 }
 
 const testInput = readFile("src/day4/input.txt");
+const colorRegex = /^#([0-9a-f]{6})$/;
+const eclValues = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+const pidRegex = /^\d{9}$/;
+
+
+function isValidRange(min, max, value) {
+    return value >= min && value <= max
+}
+
+
+function isValid(data, fieldName) {
+    let value = data.get(fieldName);
+
+    switch (fieldName) {
+        case "byr":
+            value = parseInt(value, 10);
+            return isValidRange(1920, 2002, value);
+        case "iyr":
+            value = parseInt(value, 10);
+            return isValidRange(2010, 2020, value);
+        case "eyr":
+            value = parseInt(value, 10);
+            return isValidRange(2020, 2030, value);
+        case "hgt":
+            const num = parseInt(value, 10);
+            if (value.endsWith("cm")) {
+                return isValidRange(150, 193, num);
+            } else if (value.endsWith("in")) {
+                return isValidRange(59, 76, num);
+            }
+        case "hcl":
+            return colorRegex.test(value);
+        case "ecl":
+            return eclValues.includes(value);
+        case "pid":
+            return pidRegex.test(value);
+        default:
+            return false;
+    }
+}
 
 function solution(input) {
     let result = 0;
     input.forEach(passport => {
+        passport = passport.replace(/\n/g, " ");
+        const data = new Map(passport.split(" ").map(s => s.split(":")));
+        let invalid = [];
         if (REQUIRED_FIELDS.every(field => {
-            return passport.includes(field);
+            const valid = passport.includes(field) && isValid(data, field);
+            if (!valid) {
+                invalid.push(field);
+            }
+            return valid;
         })) {
             result++;
+        } else {
+            console.log("==", passport, invalid);
         }
-        // console.log(passport);
     });
 
     console.log("result", result);
