b3611001bb31d2e8e1110dc20ce47ac824c95b89
Semyon Pupkov <mail@semyonpupkov.com>
2020-12-11 11:41:29 +0500

    day7p2

--- 2020/src/day7/solution.js
+++ 2020/src/day7/solution.js
@@ -21,7 +21,8 @@ function parse(data) {
 
             other.split(', ').forEach(contains => {
                 const [_, _childCount, child] = childRe.exec(contains);
-                graph[parent].push(child);
+                const data = {"color": child, "count": parseInt(_childCount, 10)};
+                graph[parent].push(data);
             });
         }
     });
@@ -33,7 +34,13 @@ function findBags(bags, bag, acc) {
     for (const parent in bags) {
         const childs = bags[parent];
 
-        if (childs.includes(bag)) {
+        const colors = [];
+        for (let i = 0; i < childs.length; i++) {
+            const color = childs[i]["color"];
+            colors.push(color);
+        }
+
+        if (colors.includes(bag)) {
             acc.add(parent);
             findBags(bags, parent, acc);
         }
@@ -42,12 +49,35 @@ function findBags(bags, bag, acc) {
     return acc;
 }
 
-function solution(data) {
+function calculateBags(bags, bag) {
+    const childs = bags[bag];
+    let total = 0;
+
+    if (childs) {
+        for (let i = 0; i < childs.length; i++) {
+            const color = childs[i]["color"];
+            const count = childs[i]["count"];
+            total += count + (count * calculateBags(bags, color));
+        }
+    }
+
+    return total;
+}
+
+function part1(data) {
     const graph = parse(data);
 
     const sum = findBags(graph, myBag, new Set()).size;
+    console.log(sum);
+}
+
+function part2(data) {
+    const graph = parse(data);
+
+    const sum = calculateBags(graph, myBag);
 
     console.log(sum);
 }
 
-solution(data);
+part1(data);
+part2(data);
